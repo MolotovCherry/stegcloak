d8ef439c027cc27e1df4c43042f13c87f6bd0806
Cherry <13651622+MolotovCherry@users.noreply.github.com>
2024-03-17 07:16:03 -0700

    Use div for clicking show/hide password button to make it easier to click

--- stegcloak-web/src/pages/home.rs
+++ stegcloak-web/src/pages/home.rs
@@ -195,27 +195,27 @@ fn Reveal(active_tab: ReadSignal<Tab>) -> impl IntoView {
                     }
                 />
 
-                // show eye
-                <svg
-                    class:hidden=move || show.get()
+                <div
+                    class="h-full flex items-center cursor-pointer"
                     on:click=move |_| {
                         set_pw_type.set(pw_type.get_untracked().flip());
-                        set_show.set(true);
+                        set_show.set(!show.get_untracked());
                     }
-                    class="w-5 h-5 flex-none cursor-pointer"
-                    xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" d="M2.036 12.322a1.012 1.012 0 0 1 0-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178Z" /><path stroke-linecap="round" stroke-linejoin="round" d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
-                </svg>
+                >
+                    // show eye
+                    <svg
+                        class:hidden=move || show.get()
+                        class="w-5 h-5 flex-none"
+                        xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" d="M2.036 12.322a1.012 1.012 0 0 1 0-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178Z" /><path stroke-linecap="round" stroke-linejoin="round" d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+                    </svg>
 
-                // hide eye
-                <svg
-                    class:hidden=move || !show.get()
-                    on:click=move |_| {
-                        set_pw_type.set(pw_type.get_untracked().flip());
-                        set_show.set(false);
-                    }
-                    class="w-5 h-5 flex-none cursor-pointer"
-                    xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" d="M3.98 8.223A10.477 10.477 0 0 0 1.934 12C3.226 16.338 7.244 19.5 12 19.5c.993 0 1.953-.138 2.863-.395M6.228 6.228A10.451 10.451 0 0 1 12 4.5c4.756 0 8.773 3.162 10.065 7.498a10.522 10.522 0 0 1-4.293 5.774M6.228 6.228 3 3m3.228 3.228 3.65 3.65m7.894 7.894L21 21m-3.228-3.228-3.65-3.65m0 0a3 3 0 1 0-4.243-4.243m4.242 4.242L9.88 9.88" />
-                </svg>
+                    // hide eye
+                    <svg
+                        class:hidden=move || !show.get()
+                        class="w-5 h-5 flex-none"
+                        xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" d="M3.98 8.223A10.477 10.477 0 0 0 1.934 12C3.226 16.338 7.244 19.5 12 19.5c.993 0 1.953-.138 2.863-.395M6.228 6.228A10.451 10.451 0 0 1 12 4.5c4.756 0 8.773 3.162 10.065 7.498a10.522 10.522 0 0 1-4.293 5.774M6.228 6.228 3 3m3.228 3.228 3.65 3.65m7.894 7.894L21 21m-3.228-3.228-3.65-3.65m0 0a3 3 0 1 0-4.243-4.243m4.242 4.242L9.88 9.88" />
+                    </svg>
+                </div>
 
             </label>
 
@@ -364,9 +364,8 @@ fn Cloak(active_tab: ReadSignal<Tab>) -> impl IntoView {
                         disabled=move || !encrypt.get()
                     />
 
-                    // show eye
-                    <svg
-                        class:hidden=move || show.get()
+                    <div
+                        class="h-full flex items-center"
                         class:cursor-pointer=move || encrypt.get()
                         on:click=move |_| {
                             // disable click if encrypt not enabled
@@ -375,29 +374,24 @@ fn Cloak(active_tab: ReadSignal<Tab>) -> impl IntoView {
                             }
 
                             set_pw_type.set(pw_type.get_untracked().flip());
-                            set_show.set(true);
+                            set_show.set(!show.get_untracked());
                         }
-                        class="w-5 h-5 flex-none"
-                        xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
-                        <path stroke-linecap="round" stroke-linejoin="round" d="M2.036 12.322a1.012 1.012 0 0 1 0-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178Z" /><path stroke-linecap="round" stroke-linejoin="round" d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
-                    </svg>
+                    >
+                        // show eye
+                        <svg
+                            class:hidden=move || show.get()
+                            class="w-5 h-5 flex-none"
+                            xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M2.036 12.322a1.012 1.012 0 0 1 0-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178Z" /><path stroke-linecap="round" stroke-linejoin="round" d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+                        </svg>
 
-                    // hide eye
-                    <svg
-                        class:hidden=move || !show.get()
-                        class:cursor-pointer=move || encrypt.get()
-                        on:click=move |_| {
-                            // disable click if encrypt not enabled
-                            if !encrypt.get_untracked() {
-                                return;
-                            }
-
-                            set_pw_type.set(pw_type.get_untracked().flip());
-                            set_show.set(false);
-                        }
-                        class="w-5 h-5 flex-none"
-                        xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" d="M3.98 8.223A10.477 10.477 0 0 0 1.934 12C3.226 16.338 7.244 19.5 12 19.5c.993 0 1.953-.138 2.863-.395M6.228 6.228A10.451 10.451 0 0 1 12 4.5c4.756 0 8.773 3.162 10.065 7.498a10.522 10.522 0 0 1-4.293 5.774M6.228 6.228 3 3m3.228 3.228 3.65 3.65m7.894 7.894L21 21m-3.228-3.228-3.65-3.65m0 0a3 3 0 1 0-4.243-4.243m4.242 4.242L9.88 9.88" />
-                    </svg>
+                        // hide eye
+                        <svg
+                            class:hidden=move || !show.get()
+                            class="w-5 h-5 flex-none"
+                            xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" d="M3.98 8.223A10.477 10.477 0 0 0 1.934 12C3.226 16.338 7.244 19.5 12 19.5c.993 0 1.953-.138 2.863-.395M6.228 6.228A10.451 10.451 0 0 1 12 4.5c4.756 0 8.773 3.162 10.065 7.498a10.522 10.522 0 0 1-4.293 5.774M6.228 6.228 3 3m3.228 3.228 3.65 3.65m7.894 7.894L21 21m-3.228-3.228-3.65-3.65m0 0a3 3 0 1 0-4.243-4.243m4.242 4.242L9.88 9.88" />
+                        </svg>
+                    </div>
                 </label>
 
                 // encrypt / hmac checkboxes
